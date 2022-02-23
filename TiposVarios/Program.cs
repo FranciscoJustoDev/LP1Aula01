b9937d97d5fdc84488170e1d74ffb45d0e296368
Rockie <92154369+FranciscoJustoDev@users.noreply.github.com>
2022-02-23 21:37:59 +0000

    Integer Types

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+            static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 0b1010;
+            int num3 = 0xA;
+
+            Console.WriteLine(num1 + ", " + num2 + ", " + num3 + ".");
+        }
+    }
+}
