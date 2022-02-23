bb1c48a0b79ff87e582afd87bda90ec281f00845
Rockie <92154369+FranciscoJustoDev@users.noreply.github.com>
2022-02-23 21:03:29 +0000

    Arguments

---
+++ Argumentos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+            static void Main(string[] args)
+        {
+            for(int i = 0; i < args.Count(); i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
