0b72f852dd8a656ee100ee386ded9d3f743ea74b
Rockie <92154369+FranciscoJustoDev@users.noreply.github.com>
2022-02-23 23:26:02 +0000

    String types. Fixed namespace TiposVarios

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Hal 9000";
+            string path_users = @"C:\Users";
+            string info = "Hey hey people!\n";
+            Console.WriteLine(name + ", " + path_users + ", " + info);
+        }
+    }
+}
