651ea744e1a46f6db59cfa196eceff9e2c0c70ec
Rockie <92154369+FranciscoJustoDev@users.noreply.github.com>
2022-02-23 21:15:23 +0000

    Summary

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Bye World!");
+﻿using System;
+
+namespace OlaMundo
+{
+    /// <summary>
+    /// This is a summary.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye World!");
+        }
+    }
+}
